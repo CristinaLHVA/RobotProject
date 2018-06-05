f7dfc8d784a3b958f0f176845d4f59d6375c3c33
unknown <meuwesr@hva.nl>
2018-06-05 16:47:05 +0200

    Padvolger begonnen

--- src/models/PadVolger.java
+++ src/models/PadVolger.java
@@ -1,5 +1,16 @@
 package models;
+import lejos.hardware.port.SensorPort;
+/**
+ *  author: Renke
+ */
+import tools.ColorTools;
 
 public class PadVolger {
+	
+	
+	public PadVolger() {
+		ColorTools padSensor = new ColorTools(SensorPort.S1);
+		padSensor.setMode(2);
+	}
 
 }
