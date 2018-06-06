3464f319c1ec8d8df639048379938a931bba8e5f
Bastiën B <bgabonsel@protonmail.com>
2018-06-06 14:24:05 +0200

    Basic padvolger geïmplementeerd.

--- src/models/PadVolger.java
+++ src/models/PadVolger.java
@@ -1,16 +1,28 @@
 package models;
+import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
+import lejos.robotics.Color;
 /**
  *  author: Renke
  */
 import tools.ColorTools;
 
-public class PadVolger {
+public class PadVolger extends TakenModule {
 	
+	ColorTools padSensor;
 	
 	public PadVolger() {
-		ColorTools padSensor = new ColorTools(SensorPort.S1);
-		padSensor.setMode(2);
+		this.padSensor = new ColorTools(SensorPort.S1);
+		padSensor.setMode("Red");
+	}
+	
+	public void leesLicht() {
+		System.out.println("Ga lezen!");
+		Button.waitForAnyPress();
+		//padSensor.setFloodLight(Color.RED);
+		float intensiteit = padSensor.getRed();	
+		System.out.println("Huidige modus: " + padSensor.getSensor().getName());
+		System.out.printf("de intensiteit is %f", intensiteit);
 	}
 
 }
