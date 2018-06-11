716935d0187bea2112fe2aab07a353474b018e68
marsmar1 <henkiestyler@hotmail.com>
2018-06-11 10:59:05 +0200

    Handler klasse aangepast met nieuwe methode en gebruik van voerUit().

--- src/models/Handler.java
+++ src/models/Handler.java
@@ -1,5 +1,11 @@
 package models;
 
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.plaf.BorderUIResource.BevelBorderUIResource;
+
+import lejos.hardware.Sound;
 import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 import tools.InfraroodTools;
@@ -9,42 +15,54 @@ public class Handler extends TakenModule {
 	InfraroodTools irSensor;
 	Verplaatsen beweeg = new Verplaatsen();
 	GrijperMotor grijper = new GrijperMotor();
+	File beer = new File("Robot-C12-D/Beerx3Mono.wav");
 
 	public Handler() {
-		// TODO Auto-generated constructor stub
-
 		this.irSensor = new InfraroodTools(SensorPort.S2);
-		// irSensor.getSensor().getRemoteCommand(1);
 	}
 
-	public void testRun() {
-		// grijper.sluit();
+	public void pakVoorwerp() {
+		// Sensor op distance mode zetten
 		irSensor.setMode(0);
-		System.out.println("Mode is: " + irSensor.getMode());
+		// Afstand opvragen en rijden
 		float distance = irSensor.getRange();
-		System.out.println(irSensor.getRange());
-		while (distance > 33) {
+		while (distance > 30) {
 			beweeg.motorPower(50, 50);
 			beweeg.rijVooruit();
 			distance = irSensor.getRange();
 		}
-
+		// Stoppen binnen bepaalde afstand
 		beweeg.motorPower(0, 0);
 		beweeg.rijVooruit();
+		// Armen openen
 		grijper.open();
+		// Stukje vooruit rijden
 		beweeg.motorPower(20, 20);
 		beweeg.rijVooruit();
 		Delay.msDelay(2400);
+		// Stoppen
 		beweeg.motorPower(0, 0);
 		beweeg.rijVooruit();
+		// Armen weer sluiten
 		grijper.sluit();
-		beweeg.motorPower(40, 60);
+		// Draaien naar de originele rijrichting
+		beweeg.motorPower(50, -50);
 		beweeg.rijVooruit();
-		Delay.msDelay(4400);
+		Delay.msDelay(1500);
+		// Stoppen met draaien en recht zetten
+		beweeg.motorPower(0, 0);
+		beweeg.rijVooruit();
+		beweeg.motorPower(-50, 50);
+		beweeg.rijVooruit();
+		Delay.msDelay(100);
+		beweeg.motorPower(0, 0);
+		beweeg.rijVooruit();
+		// Terug rijden + 'Beer beer beer' afspelen
 		beweeg.motorPower(50, 50);
 		beweeg.rijVooruit();
+		Sound.beepSequenceUp();
 		Delay.msDelay(5000);
-
+		// Stoppen en resources afsluiten
 		beweeg.motorPower(0, 0);
 		beweeg.rijVooruit();
 
@@ -52,14 +70,63 @@ public class Handler extends TakenModule {
 		irSensor.close();
 	}
 
+	public void zoekVoorwerp() {
+		// Sensor in beacon modus zetten
+		irSensor.setMode(1);
+		// Zoeken naar de beacon
+		// Afstand en richting van beacon opvragen (kanaal 3)
+		float distance = irSensor.getBeacon()[5];
+		// Rijden naar de beacon !! met bijsturen waar nodig !!
+		while (distance > 38) {
+			if (distance > 55) {
+				beweeg.motorPower(30, 30);
+				beweeg.rijVooruit();
+				distance = irSensor.getBeacon()[5];
+			} else if (distance < 55 && distance > 38) {
+				beweeg.motorPower(20, 20);
+				beweeg.rijVooruit();
+				distance = irSensor.getBeacon()[5];
+			} else {
+				break;
+			}
+		}
+		// Stoppen en armen openen
+		beweeg.motorPower(0, 0);
+		beweeg.rijVooruit();
+		grijper.open();
+		// Stukje vooruit rijden
+		beweeg.motorPower(20, 20);
+		beweeg.rijVooruit();
+		Delay.msDelay(2400);
+		// Stoppen en armen sluiten
+		beweeg.motorPower(0, 0);
+		beweeg.rijVooruit();
+		grijper.sluit();
+		// Zoeken naar de beacon
+		// Draaien in de goede richting (tweede beacon?)
+		// Terug rijden en 'beer beer beer' afspelen !! met bijsturen !!
+		beweeg.motorPower(50, 50);
+		beweeg.rijVooruit();
+		Sound.playSample(beer);
+		Delay.msDelay(5000);
+		// Stoppen
+		beweeg.motorPower(0, 0);
+		beweeg.rijVooruit();
+
+		beweeg.stop();
+		irSensor.close();
+
+	}
+
 	public void testBeacon() {
+
 		irSensor.setMode(1);
 		// irSensor.getBeacon();
-//		float[] beacon = { irSensor.getBeacon()[4], irSensor.getBeacon()[5] };
+		// float[] beacon = { irSensor.getBeacon()[4], irSensor.getBeacon()[5] };
 		float distance = irSensor.getBeacon()[1];
-//		for (int i = 0; i < beacon.length; i++) {
-//			System.out.println(beacon[i]);
-//		}
+		// for (int i = 0; i < beacon.length; i++) {
+		// System.out.println(beacon[i]);
+		// }
 		while (distance > 38) {
 			if (distance > 55) {
 				beweeg.motorPower(30, 30);
@@ -91,7 +158,7 @@ public class Handler extends TakenModule {
 		Delay.msDelay(5000);
 		beweeg.motorPower(0, 0);
 		beweeg.rijVooruit();
-		
+
 		beweeg.stop();
 		irSensor.close();
 
@@ -140,7 +207,7 @@ public class Handler extends TakenModule {
 
 	@Override
 	public void voerUit() {
-		// TODO Auto-generated method stub
+		pakVoorwerp();
 
 	}
 
@@ -149,6 +216,5 @@ public class Handler extends TakenModule {
 		// TODO Auto-generated method stub
 
 	}
-	
-	
+
 }
