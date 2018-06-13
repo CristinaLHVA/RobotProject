1b4359ccb27add940449e1389fac650879092b09
Bastiën B <bgabonsel@protonmail.com>
2018-06-13 09:17:15 +0200

    Constanten toegevoegd aan MusicPlayer

--- src/models/MusicPlayer.java
+++ src/models/MusicPlayer.java
@@ -12,6 +12,19 @@ public class MusicPlayer extends TakenModule implements Runnable {
 	int mode;
 	public final static int RPS_MODE = 1;
 	public final static int PADVOLGER_MODE = 2;
+	public final static int G = 784;
+	public final static int F = 698;
+	public final static int D = 587;
+	public final static int C = 523;
+	public final static int B = 494;
+	public final static int Bb = 466; 
+	public final static int A = 440;
+	public final static int shortDur = 150;
+	public final static int midDur = 333;
+	public final static int longDur = 1200;
+	public final static int defaultVol = 100;
+	
+	
 
 	public MusicPlayer() {
 		// Deze is nodig om de RPS-muziek af te spelen
@@ -29,28 +42,28 @@ public class MusicPlayer extends TakenModule implements Runnable {
 	// Vooralsnog geprobeerd om constanten aan te maken van de tonen, echter,
 	// Eclipse beweert dan dat er duplicate fields zijn
 	public void sadMel() {
-		Sound.playTone(523, 180, 75);
-		Sound.playTone(494, 180, 75);
-		Sound.playTone(466, 180, 75);
-		Sound.playTone(440, 180, 75);
+		Sound.playTone(C, shortDur, defaultVol);
+		Sound.playTone(B, shortDur, defaultVol);
+		Sound.playTone(Bb, shortDur, defaultVol);
+		Sound.playTone(A, shortDur, defaultVol);
 	}
 
 	public void happyMel() {
-		Sound.playTone(523, 300, 75);
-		Sound.playTone(523, 125, 75);
-		Sound.playTone(523, 125, 75);
-		Sound.playTone(784, 1200, 75);
+		Sound.playTone(C, midDur, defaultVol);
+		Sound.playTone(C, shortDur, defaultVol);
+		Sound.playTone(C, shortDur, defaultVol);
+		Sound.playTone(G, longDur, defaultVol);
 	}
 
 	public void neutralMel() {
-		Sound.playTone(523, 333, 75);
-		Sound.playTone(440, 333, 75);
+		Sound.playTone(C, midDur, defaultVol);
+		Sound.playTone(A, midDur, defaultVol);
 	}
 
 	public void bewegingKlaarMel() {
-		Sound.playTone(523, 333, 75);
-		Sound.playTone(587, 333, 75);
-		Sound.playTone(698, 333, 75);
+		Sound.playTone(C, midDur, defaultVol);
+		Sound.playTone(D, midDur, defaultVol);
+		Sound.playTone(F, midDur, defaultVol);
 	}
 
 	@Override
